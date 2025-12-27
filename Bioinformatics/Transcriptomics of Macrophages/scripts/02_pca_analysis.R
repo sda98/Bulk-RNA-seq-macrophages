a15c3af5a5a468003a8470b6215b26148a73159a
Sergey Dadoyan <145821999+sda98@users.noreply.github.com>
2025-12-27 15:53:17 -0700

    Update 02_pca_analysis.R

--- Bioinformatics/Transcriptomics of Macrophages/scripts/02_pca_analysis.R
+++ Bioinformatics/Transcriptomics of Macrophages/scripts/02_pca_analysis.R
@@ -36,15 +36,15 @@ library(grid)
 vsd <- vst(dds, blind = TRUE)
 
 
-# ---- PCA computation ----
-# PCA plots
+# ---- Principal Component Analysis ----
+
+# Preparing data
 
 ntop <- 500
 rv <- matrixStats::rowVars(assay(vsd))
 top_idx <- order(rv, decreasing = TRUE)[seq_len(min(ntop, length(rv)))]
 
 pca <- prcomp(t(assay(vsd)[top_idx, ]), center = TRUE, scale. = FALSE)
-pca_loadings <- pca$rotation
 
 pca_data <- data.frame(
   Sample = rownames(pca$x),
@@ -60,6 +60,7 @@ pc_variance <- (pca$sdev^2) / sum(pca$sdev^2) * 100
 
 
 # ---- Plot settings (colors / shapes) ----
+
 pol_cols <- c("M0"="red",
               "M1"="blue",
               "M2 (IL10)"="purple",
@@ -68,6 +69,10 @@ pol_cols <- c("M0"="red",
 
 
 don_shapes <- c(21, 22, 24)
+
+pca_data$Polarization <- factor(pca_data$Polarization, levels = names(pol_cols))
+
+pca_data$Donor <- factor(pca_data$Donor)  
 
 
 # ============================================================
@@ -82,7 +87,6 @@ p <- ggplot(pca_data, aes(x = PC1, y = PC2)) +
              stroke = 0.5, size = 5.5, color = "black",
              alpha = 0.7) +
   scale_fill_manual(values = pol_cols) +
-  scale_color_manual(values = pol_cols) +
   scale_shape_manual(values = don_shapes) +
   scale_y_continuous(limits = c(-50, 125), breaks = seq(-50, 125, by = 25)) +
   scale_x_continuous(limits = c(-50, 125), breaks = seq(-50, 125, by = 25)) +
@@ -95,15 +99,12 @@ p <- ggplot(pca_data, aes(x = PC1, y = PC2)) +
     axis.text = element_text(size = 22, color = "black"),
     axis.title.y = element_text(size = 22, face = "bold"),
     axis.title.x.bottom = element_text(size = 22, face = "bold",
-                                margin = margin(t = 15, unit = "pt")),
+                                       margin = margin(t = 15, unit = "pt")),
     legend.position = "none"
   ) +
   coord_fixed(ratio = 1)
 
 ## Creating the legend
-
-pca_data$Polarization <- factor(pca_data$Polarization, levels = names(pol_cols))
-pca_data$Donor <- factor(pca_data$Donor)  
 
 donor_levels <- levels(pca_data$Donor)
 donor_nums <- setNames(as.character(seq_along(donor_levels)), donor_levels)
@@ -114,23 +115,21 @@ legend_df <- expand.grid(
   KEEP.OUT.ATTRS = FALSE
 )
 
-
 legend_panel <- ggplot(legend_df, aes(x = Donor, y = Polarization, shape = Donor, fill = Polarization)) +
   geom_point(size = 6, stroke = 0.5, color = "black", alpha = 0.7) + 
   scale_fill_manual(values = pol_cols) + 
   scale_shape_manual(values = don_shapes) + 
   scale_x_discrete(position = "top", labels = donor_nums) +
-  scale_y_discrete(position = "left", labels = donor_nums) +
   labs(x = "Donor", y = NULL) + 
   theme_minimal() + 
   theme(panel.grid = element_blank(), 
-  axis.text.x.top = element_text(size = 21, color = "black",
-                             margin = margin(b = -2, unit = "pt")), 
-  axis.text.y = element_text(size = 15, color = "black"),
-  axis.title.x.top = element_text(size = 18, color = "black", face = "bold",
-                              margin = margin(b = 8, unit = "pt")),
-  axis.title.y = element_text(size = 12, color = "black", face = "bold"),
-  plot.margin = margin(0, 0, 0, 0), legend.position = "none")
+        axis.text.x.top = element_text(size = 21, color = "black",
+                                       margin = margin(b = -2, unit = "pt")), 
+        axis.text.y = element_text(size = 15, color = "black"),
+        axis.title.x.top = element_text(size = 18, color = "black", face = "bold",
+                                        margin = margin(b = 8, unit = "pt")),
+        legend.position = "none")
+
 
 ## Final plot
 
@@ -149,12 +148,11 @@ ggsave("PC1_vs_PC2.png", plot = PC1_vs_PC2,
 
 ## Creating the main plot
 
-p <- ggplot(pca_data, aes(x = PC3, y = PC4)) +
+p_2 <- ggplot(pca_data, aes(x = PC3, y = PC4)) +
   geom_point(aes(shape = Donor, fill = Polarization),
              stroke = 0.5, size = 5.5, color = "black",
              alpha = 0.7) +
   scale_fill_manual(values = pol_cols) +
-  scale_color_manual(values = pol_cols) +
   scale_shape_manual(values = don_shapes) +
   scale_y_continuous(limits = c(-30, 50), breaks = seq(-30, 50, by = 20)) +
   scale_x_continuous(limits = c(-30, 50), breaks = seq(-30, 50, by = 20)) +
@@ -173,42 +171,9 @@ p <- ggplot(pca_data, aes(x = PC3, y = PC4)) +
   coord_fixed(ratio = 1)
 
 
-## Creating the legend
-
-pca_data$Polarization <- factor(pca_data$Polarization, levels = names(pol_cols))
-pca_data$Donor <- factor(pca_data$Donor)  
-
-donor_levels <- levels(pca_data$Donor)
-donor_nums <- setNames(as.character(seq_along(donor_levels)), donor_levels)
-
-legend_df <- expand.grid(
-  Donor = donor_levels,
-  Polarization = levels(pca_data$Polarization),
-  KEEP.OUT.ATTRS = FALSE
-)
-
-
-legend_panel <- ggplot(legend_df, aes(x = Donor, y = Polarization, shape = Donor, fill = Polarization)) +
-  geom_point(size = 6, stroke = 0.5, color = "black", alpha = 0.7) + 
-  scale_fill_manual(values = pol_cols) + 
-  scale_shape_manual(values = don_shapes) + 
-  scale_x_discrete(position = "top", labels = donor_nums) +
-  scale_y_discrete(position = "left", labels = donor_nums) +
-  labs(x = "Donor", y = NULL) + 
-  theme_minimal() + 
-  theme(panel.grid = element_blank(), 
-        axis.text.x.top = element_text(size = 21, color = "black",
-                                       margin = margin(b = -2, unit = "pt")), 
-        axis.text.y = element_text(size = 15, color = "black"),
-        axis.title.x.top = element_text(size = 18, color = "black", face = "bold",
-                                        margin = margin(b = 8, unit = "pt")),
-        axis.title.y = element_text(size = 12, color = "black", face = "bold"),
-        plot.margin = margin(0, 0, 0, 0), legend.position = "none")
-
-
 ## Final plot
 
-PC3_vs_PC4 <- (p | legend_panel + plot_layout(heights = c(1, 2))) +
+PC3_vs_PC4 <- (p_2 | legend_panel + plot_layout(heights = c(1, 2))) +
   plot_layout(widths = c(6, 1))
 PC3_vs_PC4
 
@@ -224,7 +189,10 @@ ggsave("PC3_vs_PC4.png", plot = PC3_vs_PC4,
 
 ### Loadings matrix (genes x PCs) 
 
+pca_loadings <- pca$rotation
+
 gene_ids_raw <- rownames(pca_loadings)
+
 
 ###  Detect ID type in rownames 
 
@@ -234,7 +202,7 @@ is_numeric <- all(grepl("^\\d+$", gene_ids_raw))
 keytype <- if (is_ensembl) "ENSEMBL" else if (is_numeric) "ENTREZID" else "SYMBOL"
 
 ### Strip Ensembl version suffix 
-  
+
 gene_key <- if (keytype == "ENSEMBL") sub("\\..*$", "", gene_ids_raw) else gene_ids_raw
 
 ### Annotation table 
@@ -276,8 +244,6 @@ load_wide <- as.data.frame(pca_loadings[, pcs, drop = FALSE]) %>%
   mutate(
     SYMBOL_clean = dplyr::if_else(!is.na(SYMBOL) & SYMBOL != "", SYMBOL, gene_id)
   ) %>%
-  rowwise() %>%
-  ungroup() %>%
   dplyr::select(gene_id, ENSEMBL, SYMBOL, GENENAME, SYMBOL_clean, starts_with("PC"))
 
 ### Saving loadings table
@@ -291,17 +257,17 @@ readr::write_csv(load_wide, "PC_loadings.csv")
 
 ## TOP PC loadings bar plot
 
-pcs <- intersect(c("PC1","PC2","PC3","PC4"), names(load_wide))
+pcs_loadings <- intersect(c("PC1","PC2","PC3","PC4"), names(load_wide))
 stopifnot(length(pcs) > 0)
 
 plot_df <- load_wide %>%
-  dplyr::select(SYMBOL_clean, all_of(pcs)) %>%
-  pivot_longer(all_of(pcs), names_to = "PC", values_to = "loading") %>%
+  dplyr::select(SYMBOL_clean, all_of(pcs_loadings)) %>%
+  pivot_longer(all_of(pcs_loadings), names_to = "PC", values_to = "loading") %>%
   mutate(abs_loading = abs(loading)) %>%
   group_by(PC) %>%
-  slice_max(abs_loading, n = 10, with_ties = FALSE) %>%
+  slice_max(abs_loading, n = 10, with_ties = TRUE) %>%
   ungroup() %>%
-  mutate(SYMBOL = reorder_within(SYMBOL_clean, abs_loading, PC))   # <-- ascending within PC
+  mutate(SYMBOL = reorder_within(SYMBOL_clean, abs_loading, PC))   
 
 loadings_plot <- ggplot(plot_df, aes(SYMBOL, abs_loading)) +
   geom_col(fill = "#4E79A7", color = "black", linewidth = 0.35, width = 0.75) +
@@ -314,7 +280,7 @@ loadings_plot <- ggplot(plot_df, aes(SYMBOL, abs_loading)) +
     x = "Genes",
     y = "Absolute Loading"
   ) +
-  theme_classic(base_size = 12) +
+  theme_classic() +
   theme(
     plot.title = element_text(face = "bold", hjust = 0.5, color = "black", size = 35),
     plot.subtitle = element_text(hjust = 0.5, color = "black", size = 29),
